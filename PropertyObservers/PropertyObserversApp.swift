eaa590ebc4b869c8b2d81449073023ad85f7a15b
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-04-14 00:05:14 +0600

    project init

---
+++ PropertyObservers/PropertyObserversApp.swift
@@ -0,0 +1,17 @@
+//
+//  PropertyObserversApp.swift
+//  PropertyObservers
+//
+//  Created by Shah Md Imran Hossain on 13/4/23.
+//
+
+import SwiftUI
+
+@main
+struct PropertyObserversApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
