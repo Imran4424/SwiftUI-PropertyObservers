ad2b94a7a0aae0a37fe37cef14c59047c7836c42
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-09-03 17:33:58 +0600

    external state

--- PropertyObservers/BindingProperty.swift
+++ PropertyObservers/BindingProperty.swift
@@ -7,9 +7,43 @@
 
 import SwiftUI
 
-struct BindingProperty: View {
+struct PushButton: View {
+    let title: String
+    // state will not work here
+    // state only modify value locally
+    // does not send back the value
+//    @State var isOn: Bool
+    // Binding not only modify the value locally
+    // also send it back to the calling place
+    // Binding is called exteral state
+    @Binding var isOn: Bool
+    
+    
+    var onColors = [Color.red, Color.yellow]
+    var offColors = [Color(white: 0.6), Color(white: 0.4)]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(title) {
+            isOn.toggle()
+        }
+        .padding()
+        .background(
+            LinearGradient(colors: isOn ? onColors: offColors, startPoint: .top, endPoint: .bottom)
+        )
+        .foregroundColor(.white)
+        .clipShape(Capsule())
+        .shadow(radius: isOn ? 0 : 5)
+    }
+}
+
+struct BindingProperty: View {
+    @State private var rememberMe = false
+    
+    var body: some View {
+        VStack {
+            PushButton(title: "Remember Me", isOn: $rememberMe)
+            Text(rememberMe ? "On" : "Off")
+        }
     }
 }
 
