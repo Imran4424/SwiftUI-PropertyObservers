8a19d4ef91ed00add155b4b7ce6032d5d365e219
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-09-03 16:39:51 +0600

    binding property

---
+++ PropertyObservers/BindingProperty.swift
@@ -0,0 +1,20 @@
+//
+//  BindingProperty.swift
+//  PropertyObservers
+//
+//  Created by Shah Md Imran Hossain on 3/9/23.
+//
+
+import SwiftUI
+
+struct BindingProperty: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BindingProperty_Previews: PreviewProvider {
+    static var previews: some View {
+        BindingProperty()
+    }
+}
